4b88fd5872ec5e149c201aa3f1d726098477f913
Andrew1412Belous <andrew.belous1412@gmail.com>
2023-03-10 12:08:39 +0200

    add ref

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -76,16 +76,41 @@ class CharList extends Component {
     })
   }
 
+  itemRefs = []
+
+  setRef = (ref) => {
+    this.itemRefs.push(ref)
+  }
+
+  focusOnItem = (id) => {
+    this.items
+      .forEach(item => item.classList.remove('char__item_selected'))
+
+    this.itemRefs[id].classList.add('char__item_selected')
+    this.itemRefs[id].focus()
+  }
+
   renderChars = (items) => {
-    const chars = items.map(item => {
+    const chars = items.map((item, index) => {
       const imgStyle = item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
         ? { objectFit: 'contain' }
         : { objectFit: 'cover' }
 
       return (
         <li className="char__item"
+            tabIndex={0}
+            ref={this.setRef}
             key={item.id}
-            onClick={() => this.props.onCharSelected(item.id)}>
+            onClick={() => {
+              this.props.onCharSelected(item.id)
+              this.focusOnItem(item.id)
+            }}
+            onKeyPress={(e) => {
+              if (e.key === ' ' || e.key === "Enter") {
+              this.props.onCharSelected(item.id);
+              this.focusOnItem(index);
+            }
+            }}>
           <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
           <div className="char__name">{item.name}</div>
         </li>
